4d815d261a34ece312bccb3a508f98b63a09b2c3
TaxAndria1213 <105701182+TaxAndria1213@users.noreply.github.com>
2022-10-05 12:54:41 +0300

    ajout composant livres disponibles

---
+++ src/Controleur/Livres.java
@@ -0,0 +1,100 @@
+package Controleur;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+
+import Modele.Interface_donne_static;
+
+/**
+ * Livres
+ */
+public class Livres implements Interface_donne_static{
+    String insertion_error;
+    public Livres(){
+    }
+    //Ajout nouveau liste dans le fichier avec Buffered
+    static void ecriture_dans_le_fichier(String nom_liv ){
+        try {
+            FileOutputStream ecriture_donnee = new FileOutputStream(fichier_livre, true);
+            byte[] nom_des_livres = nom_liv.getBytes();
+            ecriture_donnee.write(nom_des_livres);
+            ecriture_donnee.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+            System.err.println(e);
+        }
+        
+    }
+    static void ajout_livre(String nom_du__livre_a_ajouter)
+    {
+        try {
+            if (fichier_livre.createNewFile()) {
+                ecriture_dans_le_fichier(nom_du__livre_a_ajouter);
+            } else {
+                ecriture_dans_le_fichier(nom_du__livre_a_ajouter);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    } 
+    public void nouvelle_livre(String nouv_livre) {
+        Boolean test_donner_existant = false;
+        for (int i = 0; i < nomLivre.size(); i++) {
+            if (nouv_livre.equals(nomLivre.get(i))) {
+                test_donner_existant = true;
+                break;
+            }
+        }
+        if (test_donner_existant == true) {
+            System.out.println("cette livre existe deja");
+        } else {
+            ajout_livre(nouv_livre);
+        }    
+    }
+    public void Lecture(String livres){
+        Interface_donne_static.nomLivre.add(livres);
+    }
+
+    public void livre_env_dans_array() {
+        try (BufferedReader lecture_fichier_livre = new BufferedReader(new FileReader(fichier_livre)))
+        {
+            String les_livres;
+            while ((les_livres = lecture_fichier_livre.readLine()) != null) 
+            {
+                Lecture(les_livres);
+            }
+        } catch (IOException e) {
+            System.out.println("erreur de selection");
+            e.printStackTrace();
+        }
+    }
+
+    public String nom_du_livre_emprt(String nom_liv)
+    {
+        if (nom_liv != null) {
+            return nom_liv;  
+        } else {
+           return null; 
+        }
+    }
+
+    public void emprinter_livre(String nom_du_livre_aempr) {
+        Boolean visibilite_livre_aempr = false;
+        for (int i = 0; i < nomLivre.size(); i++) {
+            if (nom_du_livre_aempr.equals(nomLivre.get(i)))
+            {
+                visibilite_livre_aempr = true;
+                break;
+            }
+        }
+        if (visibilite_livre_aempr == true) {
+            System.out.println("Vous avez emprenter " + nom_du_livre_aempr);
+        }
+        else
+        {
+            System.out.println(nom_du_livre_aempr + " n'existe pas dans nos librairie"); 
+        }    
+    }
+}
--- src/Vue/FenetrePrincipale.java
+++ src/Vue/FenetrePrincipale.java
@@ -2,13 +2,24 @@ package Vue;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+
+import Controleur.Livres;
+import Modele.Interface_donne_static;
 
 	
 
@@ -23,11 +34,18 @@ public class FenetrePrincipale {
 	JMenu Livre_menu = new JMenu("Livres");
 	
 	
+	
 	public FenetrePrincipale() {
+	    new Livres().livre_env_dans_array();
 	    label_emprunt_utilisateur = new JLabel("Emprunt de l'utilisateur : ");
+	    
+	    JPanel panel_label_livre_dispo = new JPanel();
 	    label_livre_dispo = new JLabel("Livre(s) disponible(s) : ");
+	    panel_label_livre_dispo.add(label_livre_dispo);
+	    
 	    label_tout_emprunts = new JLabel("Tout les emprunts : ");
 	    
+	    label_emprunt_utilisateur.setBackground(Color.WHITE);
 	    
 		framePpl = new JFrame();
 
@@ -43,15 +61,77 @@ public class FenetrePrincipale {
 		conteneur.setLayout(new GridLayout(1,3));
 		
 		JPanel pane_emprunt_utilisateur = new JPanel();
-		pane_emprunt_utilisateur.setBackground(Color.cyan);
+		pane_emprunt_utilisateur.setBackground(new Color(187,213,251));
 		pane_emprunt_utilisateur.add(label_emprunt_utilisateur);
 		
 		JPanel pane_livre_dispo = new JPanel();
-		pane_livre_dispo.setBackground(Color.gray);
-		pane_livre_dispo.add(label_livre_dispo);
+		pane_livre_dispo.setLayout(new BorderLayout());
+		pane_livre_dispo.setBackground(new Color(247, 223, 179));
+		pane_livre_dispo.add(panel_label_livre_dispo, BorderLayout.NORTH);
+		
+		//JPanel pane_liste = new JPanel();
+		
+		JPanel pane_liste_livre = new JPanel();
+		pane_liste_livre.setLayout(new GridLayout(0,2));
+		
+		
+		
+		
+		
+		
+		String fichier_live = "./Livres.txt";
+		/*try(BufferedReader br = new BufferedReader(new FileReader(fichier_live))) {
+            String line_livre;
+            int compter_livre = 0;
+            while((line_livre = br.readLine()) != null) {
+                String res_livre[] = line_livre.split(":");
+                compter_livre += 1;
+                if(res_livre[1].equals("0")) {
+                    JPanel panel_label_livre = new JPanel();
+                    JLabel label_livre = new JLabel();
+                    panel_label_livre.add(label_livre);
+                    
+                    JPanel panel_btn_emprunter = new JPanel();
+                    JButton btn_emprunter = new JButton("Emprunter");
+                    panel_btn_emprunter.add(btn_emprunter);
+                    
+                    pane_liste_livre.add(panel_label_livre);
+                    pane_liste_livre.add(panel_btn_emprunter);
+                    pane_livre_dispo.add(pane_liste_livre, BorderLayout.CENTER);
+                    
+                    JList<Object> livre = new JList<Object>();
+                                        
+                }
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }*/
+		for (int i = 0; i < Interface_donne_static.nomLivre.size(); i++) {
+            JPanel panel_label_livre = new JPanel();
+            JLabel label_livre = new JLabel(Interface_donne_static.nomLivre.get(i));
+            panel_label_livre.add(label_livre);
+            
+            JPanel panel_btn_emprunter = new JPanel();
+            JButton btn_emprunter = new JButton("Emprunter");
+            panel_btn_emprunter.add(btn_emprunter);
+            int identifiant = i;
+            
+            btn_emprunter.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    // TODO Auto-generated method stub
+                    System.out.println(Interface_donne_static.nomLivre.get(identifiant));
+                }
+            });
+            
+            pane_liste_livre.add(panel_label_livre);
+            pane_liste_livre.add(panel_btn_emprunter);
+            pane_livre_dispo.add(pane_liste_livre, BorderLayout.CENTER);
+        }  
+		
 		
 		JPanel pane_tout_les_emprunts = new JPanel();
-		pane_tout_les_emprunts.setBackground(Color.GREEN);
+		pane_tout_les_emprunts.setBackground(new Color(185, 248, 175));
 		pane_tout_les_emprunts.add(label_tout_emprunts);
 		
 		
@@ -65,7 +145,7 @@ public class FenetrePrincipale {
 		
 		framePpl.setContentPane(paneau_principal);
 		framePpl.setSize(1080, 720);
-		framePpl.setResizable(false);
+		//framePpl.setResizable(false);
 		framePpl.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		framePpl.setLocationRelativeTo(null);
 		framePpl.setVisible(true);
